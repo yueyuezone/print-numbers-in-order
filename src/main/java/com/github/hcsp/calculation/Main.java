4973cab1064c9cc586f1734f94ec67f5b1b82d0a
shiliyu <34638563+shiliyu@users.noreply.github.com>
2019-06-16 20:08:37 +0800

    print numbers in order (#11)
    print numbers in order (#11)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.*;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +15,11 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        List<Integer> list = Arrays.asList(a, b, c);
+        Collections.sort(list);
+        return list.get(2) + ">" + list.get(1) + ">" + list.get(0);
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
