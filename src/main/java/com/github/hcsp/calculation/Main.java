b62bd04d9d370b8186b97b32920437a78ddc7273
余韵之 <36914785+Lambert1129@users.noreply.github.com>
2019-06-17 17:48:29 +0800

    x (#12)
    x (#12)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.lang.reflect.Array;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,8 +15,38 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
 
+        String result=null;
+        int[] nums = new int[] {a,b,c};
+        bubbleSort(nums);
+
+        for (int i=0;i<nums.length;i++) {
+            if(i== 0){
+                result =nums[i]+">";
+            }else if(i!=(nums.length-1)){
+                result += nums[i] + ">";
+            }else{
+                result += nums[i];
+            }
+
+        }
+
+        return result;
+    }
+    public static void bubbleSort(int[] nums) {
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j < (len - i); j++) {
+                if (nums[j - 1] < nums[j]) {
+                    int temp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
         System.out.println(printNumbersInOrder(-1, 2, -3));
