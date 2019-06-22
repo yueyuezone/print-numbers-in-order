c52cd6e7771e2dc61bf7a07211eae407f13628c9
dingsheng <dingduoduo0916@gmail.com>
2019-06-23 00:23:11 +0800

    异或运算符^ 的用法 x^y^y = x

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.calculation;
 
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +14,12 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+      Integer max = Math.max(Math.max(a, b), c);
+      Integer min = Math.min(Math.min(a, b), c);
+      Integer mid = a^b^c^max^min;// x^y^y = x
+      return max.toString() + '>' + mid.toString() + '>' + min.toString();
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
