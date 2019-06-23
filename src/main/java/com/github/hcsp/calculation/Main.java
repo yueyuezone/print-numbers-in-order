a9e7e47274ecd55570e22c46450a070997e6f0fa
DeeJayStudy <52043001+DeeJayStudy@users.noreply.github.com>
2019-06-24 00:21:35 +0800

    sort (#19)
    sort (#19)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,24 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if(a < b) {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        if(a < c) {
+            int temp = c;
+            c = a;
+            a = temp;
+        }
+        if(b < c) {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+        return a + ">" + b + ">" + c;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
