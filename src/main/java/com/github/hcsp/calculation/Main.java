89df039669e6f4746731cf6b7e3c0a9594a76db2
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-06-30 16:37:38 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,22 +1,23 @@
-package com.github.hcsp.calculation;
+package main.java.com.github.hcsp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
-    /**
-     * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
-     *
-     * <p>例如，a=1,b=3,c=2，返回字符串"3>2>1"。
-     *
-     * <p>又如，a=-1,b=100,c=4，返回字符串"100>4>-1"
-     *
-     * @param a 数字一
-     * @param b 数字二
-     * @param c 数字三
-     * @return 所要求的字符串
-     */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int[] intArr = {a, b, c};
+        Arrays.sort(intArr);
+        String[] stringArr = new String[intArr.length];
+        for (int i = 0; i < intArr.length; i++) stringArr[i] = String.valueOf(intArr[i]);
+        ArrayList<String> strList = new ArrayList<>(Arrays.asList(stringArr));
+        Collections.reverse(strList);
+        return String.join(">", strList);
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
         System.out.println(printNumbersInOrder(-1, 2, -3));
     }
 }
+
