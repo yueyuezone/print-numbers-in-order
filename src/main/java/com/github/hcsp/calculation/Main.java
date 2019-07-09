e348e773a0b511fcce57ba14992555f9ea5b287b
Zhang Xin <zhangxindev@gmail.com>
2019-07-09 22:16:22 +0800

    按要求完成方法

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,20 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int max, med, min;
+        max = Math.max(a, b) > c ? Math.max(b, c) : c;
+        min = Math.min(a, b) < c ? Math.min(a, b) : c;
+        if (a != max && a != min) {
+            med = a;
+        } else if (b != max && b != min) {
+            med = b;
+        } else {
+            med = c;
+        }
+
+        return max + ">" + med + ">" + min;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
