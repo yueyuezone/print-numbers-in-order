b0ca2acc78b9ef6d9724a69d63e661bc07a317da
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-24 18:25:03 +0800

    Update Main.java (#39)
    Update Main.java (#39)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,13 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int start = Math.max(Math.max(a,b),c);
+        int tail = Math.min(Math.min(a,b),c);
+        int pivot = a + b + c - start -tail;
+        return start + ">" + pivot + ">" + tail;
+    }
+    
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
