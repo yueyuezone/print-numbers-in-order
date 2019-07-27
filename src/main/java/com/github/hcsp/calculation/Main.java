362211502baad14b3a84b65fcae89622bce8087a
taoxm310 <taoxiumin2016@gmail.com>
2019-07-27 17:22:56 +0800

    print-numbers-in-order (#40)
    print-numbers-in-order (#40)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,14 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int max = Math.max(Math.max(a,b),c);
+        int min = Math.min(Math.min(a,b),c);
+        int middle = a + b + c - min - max;
+        return String.format("%d>%d>%d",max, middle,min);
+
+
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
