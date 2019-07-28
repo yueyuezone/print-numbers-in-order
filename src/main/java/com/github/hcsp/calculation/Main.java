da6ebdf546f4363227013b547cf3e151dbdd6f1b
fei tang <497045233@qq.com>
2019-07-28 21:58:13 +0800

    按照顺序打印数字 (#41)
    按照顺序打印数字 (#41)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,32 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        String result="";
+        if(a>b&&a>c){
+            result+=a+">";
+            if(b>c){
+                result+=b+">"+c;
+            }else{
+                result+=c+">"+b;
+            }
+        }else if(b>a&&b>c){
+            result+=b+">";
+            if(a>c){
+                result+=a+">"+c;
+            }else{
+                result+=c+">"+a;
+            }
+        }else if(c>a&&c>b){
+            result+=c+">";
+            if(a>b){
+                result+=a+">"+b;
+            }else{
+                result+=b+">"+a;
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
